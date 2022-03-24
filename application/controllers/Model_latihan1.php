aeb657ed38e7df383ae9bc7a6e21ab2f41bad697
triindriyani15 <trieyarsi@gmail.com>
2022-03-24 19:30:51 +0700

    penambahan isi model_latihan.php

--- application/controllers/Model_latihan1.php
+++ application/controllers/Model_latihan1.php
@@ -1,10 +1,10 @@
 <?php
 class Model_latihan1 extends CI_Model
 {
-    //membuat variabel untuk menampung nilai
+    //membuat variabel untuk menampung nilai.
     public $nilai1, $nilai2, $hasil1;
 
-    //method penjumlahan
+    //method penjumlahan.
     public function jumlah($n1 = null, $n2 = null)
     {
         $this->nilai1 = $nil1;
