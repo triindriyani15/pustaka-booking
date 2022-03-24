43041aa194aecbbdaf333257408130f497ff67db
triindriyani15 <trieyarsi@gmail.com>
2022-03-24 19:25:51 +0700

    penambahan isi contoh1.php

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,6 +4,6 @@ class Contoh1 extends CI_Controller
         public function index()
         {
             echo "<h1>Perkenalkan</h1>";
-            echo "Nama saya Imam Namawi Saya tinggal di aderah Ciputat olahraga yang saya sukai adalah Bulutangkis";
+            echo "Nama saya Imam Namawi. Saya tinggal di aderah Ciputat olahraga yang saya sukai adalah Bulutangkis";
         }
 }
