909625ac33e02be11dd6bb2a3fef2ea14ad8a9f4
triindriyani15 <trieyarsi@gmail.com>
2022-03-24 19:59:23 +0700

    perubahan isi Latihan1

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -4,5 +4,14 @@ class Latihan1 extends CI_Controller
     public function index()
     {
         echo "Selamat Datang... selamat belajar Web Programming";
+        //$this->load->view('view-latihan1');
+    }
+
+    public function penjumlahan ($n1, $n2)
+    {
+        $this->load->model1('Model_latihan1');
+        $hasil = $this->Model_latihan1->jumlah($n1, $n2);
+        echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = "
+    .$hasil;
     }
 }
